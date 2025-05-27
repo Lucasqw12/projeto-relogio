d8bd4a2de1d078ade84abd06f8c45ffc3f714381
Lucas M Medeiros <lucascrnmoreira@gmail.com>
2025-05-27 14:04:19 -0300

    Add files via upload

---
+++ relogio/js/script.js
@@ -0,0 +1,26 @@
+let horasInput = document.querySelector("#horas")
+let minutosInput = document.querySelector("#minutos")
+let segundosInput = document.querySelector("#segundos")
+
+
+
+
+setInterval(() => {
+    let date = new Date()
+    let horas = date.getHours()
+    let minutos = date.getMinutes()
+    let segundos = date.getSeconds()
+
+    horasInput.innerHTML = horas
+    minutosInput.innerHTML = minutos
+    segundosInput.innerHTML = segundos
+    
+    if(segundos < 10) {
+        segundosInput.innerHTML = `0${segundos}`;
+    }
+    
+    if(minutos < 10) {
+        minutosInput.innerHTML = `0${minutos}`;
+    }
+
+}, 1000);
